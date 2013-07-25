2d6c1f74b0baa958e54daba6cd41d94553ed74ba
Christine Coulter <christinetcoulter@gmail.com>
2013-07-25 09:29:48 -0400

    merge

--- examples/dom_intro/js/dom_intro.js
+++ examples/dom_intro/js/dom_intro.js
@@ -1,2 +1,14 @@
 // Traverse the DOM!
 
+function traverse_dom(search_term) {
+  // I want to traverse all the div
+  // elements in the DOM and find one whose
+  // innerText is equal to our search_team
+  var elements = document.getElementsByTagName('div');
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].innerText === search_term) {
+      return elements[i];
+    }
+  }
+  return null;
+}
