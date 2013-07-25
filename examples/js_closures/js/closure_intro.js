c6a7584d07788b8bdbaf104866ed2bba87ab62ea
Christine Coulter <christinetcoulter@gmail.com>
2013-07-25 13:46:46 -0400

    merge.

--- examples/js_closures/js/closure_intro.js
+++ examples/js_closures/js/closure_intro.js
@@ -1,18 +1,19 @@
 function handleStudents(){
   var i = 0;  // scope: local variable, type: number
-  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:???
-  return function(){  // from SO: the inner function has access to the 
-    // environment it was created within 
+  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t: array
+  return function(){  // from SO: the inner function has access to the
+    // environment it was created within
     // even after the code that created the environment has returned.
-    var student =  students[i]; // s: ???, t: string
-    i+=1; 
-    return student;// What type is this returning? 
+    var student =  students[i]; // s: local to handleStudents, t: string
+    i+=1;
+    return student;// What type is this returning? string
   };
 }
 
 // What type of variable is nextStudent?
-// t: ???
-var nextStudent = handleStudents(); 
+// t: closure (the inner function of handleStudents)
+var nextStudent = handleStudents();
+var nextStudent1 = handleStudents();
 
 // onload
 window.onload = function(){
@@ -26,6 +27,10 @@ window.onload = function(){
   };
 
   document.getElementById("resetClosure").onclick = function(){
-    nextStudent = handleStudents(); 
+    nextStudent = handleStudents();
+  };
+
+  document.getElementById("newButton").onclick = function(){
+    document.getElementById('students').innerHTML += nextStudent1() + " (from newStudent1)" + '<br/>\n';
   };
 };
