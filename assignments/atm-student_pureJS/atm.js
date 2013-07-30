d3eaec463014295a6f236367dee04127932a7754
Christine Coulter <christinetcoulter@gmail.com>
2013-07-30 15:59:18 -0400

    merge

--- assignments/atm-student_pureJS/atm.js
+++ assignments/atm-student_pureJS/atm.js
@@ -1,24 +1,35 @@
 var checkingBalance = 0;
 var savingsBalance = 0;
 
-window.onload = function(){
+$(function() {
+  $("#checking-deposit").click(getCheckingDeposit);
+  $("#savings-deposit").click(getSavingsDeposit);
+  $("#checking-withdraw").click(getCheckingWithdraw);
+  $("#getSavingsWithdraw").click(getSavingsWithdraw);
+  updateDisplay();
+});
 
-  document.getElementById('checking-deposit').onclick = function getChecking(event){
-    var amount = document.getElementById('checking-amount').value;
+// window.onload = function(){
+
+  function getCheckingDeposit(){
+    // var amount = document.getElementById('checking-amount').value;
+    var amount = $("#checking-amount").val();
     amount = parseInt(amount);
     checkingBalance = amount + checkingBalance;
     updateDisplay();
   };
 
-  document.getElementById('savings-deposit').onclick = function(event){
-    var amount = document.getElementById('savings-amount').value;
+  function getSavingsDeposit(){
+    // var amount = document.getElementById('savings-amount').value;
+    var amount = $("#savings-amount").val();
     amount = parseInt(amount);
     savingsBalance = amount + savingsBalance;
     updateDisplay();
   };
 
-  document.getElementById('checking-withdraw').onclick = function(event){
-    var amount = document.getElementById('checking-amount').value;
+  function getCheckingWithdraw(){
+    // var amount = document.getElementById('checking-amount').value;
+    var amount = $("#checking-amount").val();
     amount = parseInt(amount);
     balances = withdrawFunds(amount, checkingBalance, savingsBalance);
     checkingBalance = balances[0];
@@ -26,8 +37,9 @@ window.onload = function(){
     updateDisplay();
   };
 
-  document.getElementById('savings-withdraw').onclick = function(event){
-    var amount = document.getElementById('savings-amount').value;
+  function getSavingsWithdraw(){
+    // var amount = document.getElementById('savings-amount').value;
+    var amount = $("#savings-amount").val();
     amount = parseInt(amount);
     balances = withdrawFunds(amount, savingsBalance, checkingBalance);
     savingsBalance = balances[0];
@@ -35,9 +47,9 @@ window.onload = function(){
     updateDisplay();
   };
 
-  updateDisplay();
+  // updateDisplay();
 
-};
+// };
 
 function withdrawFunds(amount, primary, secondary) {
   if (amount <= primary) {
@@ -51,23 +63,34 @@ function withdrawFunds(amount, primary, secondary) {
 
 function updateDisplay() {
 
-  var element = document.getElementById('checking-balance');
+  // var element = document.getElementById('checking-balance');
+  var element = $("#checking-balance");
+  // element = parseInt(element);
 
   if (checkingBalance <= 0) {
-    element.classList.add('zero');
+    // element.classList.add('zero');
+    element.addClass("zero");
   } else {
-    element.classList.remove('zero');
+    // element.classList.remove('zero');
+    element.removeClass("zero");
   }
 
-  var element2 = document.getElementById('savings-balance');
+  // var element2 = document.getElementById('savings-balance');
+  var element2 = $("#savings-balance");
 
   if (savingsBalance <= 0) {
-    element2.classList.add('zero');
+    // element2.classList.add('zero');
+    element2.addClass("zero");
   } else {
-    element2.classList.remove('zero');
+    // element2.classList.remove('zero');
+    element2.removeClass("zero");
   }
-  document.getElementById('checking-balance').innerHTML = '$' + checkingBalance;
-  document.getElementById('savings-balance').innerHTML = '$' + savingsBalance;
-  document.getElementById('checking-amount').value = '';
-  document.getElementById('savings-amount').value = '';
+  // document.getElementById('checking-balance').innerHTML = '$' + checkingBalance;
+  $("#checking-balance").text('$' + checkingBalance);
+  // document.getElementById('savings-balance').innerHTML = '$' + savingsBalance;
+  $("#savings-balance").text('$' + savingsBalance);
+  // document.getElementById('checking-amount').value = '';
+  $("#checking-amount").val('');
+  // document.getElementById('savings-amount').value = '';
+  $("#savings-amount").val('');
 }
