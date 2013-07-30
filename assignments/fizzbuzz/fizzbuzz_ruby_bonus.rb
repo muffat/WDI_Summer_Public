da37fe294f3e29adb89eececc1afa92d83b3fc39
Christine Coulter <christinetcoulter@gmail.com>
2013-07-30 10:26:46 -0400

    merge

--- assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
+++ assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
@@ -1,1 +1,14 @@
-# Done in JS? Now do it in Ruby. 
+require 'pry'
+# Done in JS? Now do it in Ruby.
+numbersArray = Array(1..100)
+numbersArray.each do |number|
+  if ((number % 3 == 0) && (number % 5 == 0))
+    number = "FizzBuzz"
+  elsif (number % 3 == 0)
+    number = "Fizz"
+  elsif (number % 5 == 0)
+    number = "Buzz"
+  end
+end
+
+binding.pry
--- assignments/fizzbuzz/js/fizzbuzz.js
+++ assignments/fizzbuzz/js/fizzbuzz.js
@@ -1,1 +1,16 @@
 // Your code goes here
+var numbersArray = [];
+for (var i = 1; i <= 100; i++) {
+  numbersArray.push(i);
+}
+
+var alter = _.map(numbersArray, function(number) {
+  if ((number % 3 === 0) && (number % 5 === 0)) {
+    number = "FizzBuzz";
+  } else if (number % 3 === 0) {
+    number = "Fizz";
+  } else if (number % 5 === 0) {
+    number = "Buzz";
+  }
+  return number;
+});
